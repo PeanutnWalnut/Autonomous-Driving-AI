60d9785f78b86dc9c9572f4247951a2e0fdf771f
newmade01@naver.com <57945595+newmade01@users.noreply.github.com>
2022-06-18 21:22:07 +0900

    차선 인식

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -7,30 +7,10 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+ 
+ <p>
+    <h1> AI 자율주행 차선인식</h1>
+ </p>
 </template>
 
 <style scoped>
